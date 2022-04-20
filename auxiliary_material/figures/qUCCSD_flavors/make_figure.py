e6d136fae01cf778b2aa3db15b0ea1e8d919db1f
Mario Motta <mario@marios-mbp.almaden.ibm.com>
2022-04-19 18:03:03 -0700

    qUCCSD flavors

---
+++ auxiliary_material/figures/qUCCSD_flavors/make_figure.py
@@ -0,0 +1,105 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import itertools
+
+from matplotlib import rc
+rc('font',**{'family':'serif','serif':['cmu serif'],'size':12})
+rc('text', usetex=True)
+
+c_list = {'yellow'      : '#DAA520',
+          'orange'      : '#FF8856',
+          'red'         : '#DC343B',
+          'magenta'     : '#B284BE',
+          'purple'      : '#888FC7',
+          'blue'        : '#0079BF',
+          'cyan'        : '#5BA4CF',
+          'light-green' : '#B9D146',
+          'dark-green'  : '#32826E'}
+
+style = {'trotter_ds':{'color':c_list['red'],        's':'-', 'w':2,'marker':'o','ms':16,'label':'Trotter, d+s'},
+         'suzuki_ds' :{'color':c_list['blue'],       's':'-', 'w':2,'marker':'P','ms':16,'label':'Suzuki, d+s'},
+         'suzuki_sd' :{'color':c_list['light-green'],'s':'-.','w':2,'marker':'<','ms':13,'label':'Suzuki, s+d'},
+         'trotter_sd':{'color':c_list['orange'],     's':'-.','w':2,'marker':'s','ms':16,'label':'Trotter, s+d'}}
+
+def fill_panel(pan,xlabel,xlim,xticks,xticklabels,ylabel,ylim,yticks,yticklabels,p=20.0,q=10.0):
+    x0,x1 = xlim
+    xlim  = [x0-(x1-x0)/p,x1+(x1-x0)/p]
+    pan.set_xlabel(xlabel)
+    pan.set_xlim(xlim)
+    pan.set_xticks(xticks)
+    pan.set_xticklabels(xticklabels)
+    pan.set_ylabel(ylabel)
+    y0,y1 = ylim
+    ylim  = [y0-(y1-y0)/q,y1+(y1-y0)/q]
+    pan.set_ylim(ylim)
+    pan.set_yticks(yticks)
+    pan.set_yticklabels(yticklabels)
+    pan.tick_params(direction='in',which='both')
+
+# ------------------------------------------------------------- 
+
+R_list  = [0.7,0.9,1.1,1.3,1.5,1.7,1.9,2.1,2.3,2.5,2.7,2.9,3.1,3.3]
+flavor  = ['restricted_closed_shell','unrestricted']
+approx  = ['trotter','suzuki']
+reverse = ['doubles_singles','singles_doubles']
+title   = ['restricted','unrestricted']
+dlist   = [1,2]
+
+for depth in dlist:
+
+    L        = 4.5
+    fig,ax   = plt.subplots(2,2,figsize=(2*L,0.5*2*L))
+    fig.subplots_adjust(hspace=0.0,wspace=0.3)
+
+    res = {}
+    for f in flavor:
+        for a in approx:
+            for x in reverse:
+                res['E_vqe_%s_%s_ds_%s'%(f,a,x)] = np.zeros(len(R_list))
+                res['S_vqe_%s_%s_ds_%s'%(f,a,x)] = np.zeros(len(R_list))
+                res['E_fci_%s_%s_ds_%s'%(f,a,x)] = np.zeros(len(R_list))
+                for j,R in enumerate(R_list):
+                    DAT = np.loadtxt('../../../second_quantization/circuits/qUCCSD/%s/%s/%s/h2o_reps_%d_%s_results.txt' % (f,x,a,depth,str(R)))
+                    res['E_vqe_%s_%s_ds_%s'%(f,a,x)][j] = DAT[0][0]
+                    res['S_vqe_%s_%s_ds_%s'%(f,a,x)][j] = DAT[2][0]
+                    res['E_fci_%s_%s_ds_%s'%(f,a,x)][j] = DAT[0][1]
+    
+    for j,f in enumerate(flavor):
+        for a in approx:
+            for x in reverse:
+                E_vqe = np.array(res['E_vqe_%s_%s_ds_%s'%(f,a,x)])
+                S_vqe = np.array(res['S_vqe_%s_%s_ds_%s'%(f,a,x)])
+                E_fci = np.array(res['E_fci_%s_%s_ds_%s'%(f,a,x)])
+                if(x=='doubles_singles'): s = a+'_ds'
+                else:                     s = a+'_sd'
+                ax[0,j].plot(R_list,E_vqe-E_fci,color=style[s]['color'],label=style[s]['label'],ls=style[s]['s'],marker=style[s]['marker'],ms=5,mec='black',mew=0.5)
+                ax[1,j].plot(R_list,S_vqe,color=style[s]['color'],label=style[s]['label'],ls=style[s]['s'],marker=style[s]['marker'],ms=5,mec='black',mew=0.5)
+        ax[0,j].text(0.1,0.9,title[j],horizontalalignment='left',verticalalignment='center',transform=ax[0,j].transAxes)
+    
+    fill_panel(ax[0,0],'',[0.5,3.5],[0.5,1.0,1.5,2.0,2.5,3.0,3.5],['','','','','','',''],
+                       r'$E-E_{\mathrm{FCI}}$ [$\mathrm{E_h}$]',[0,0.09],[0.00,0.03,0.06,0.09],
+                       ['0.00','0.03','0.06','0.09'],p=20.0,q=10.0)
+    fill_panel(ax[1,0],r'$R$ [\AA]',[0.5,3.5],[0.5,1.0,1.5,2.0,2.5,3.0,3.5],[0.5,1.0,1.5,2.0,2.5,3.0,3.5],
+                       r'$S^2$ [$\hbar$]',[0,0.008],[0.00,0.002,0.004,0.006,0.008],
+                       ['0.000','0.002','0.004','0.006','0.008'],p=20.0,q=10.0)
+    
+    fill_panel(ax[0,1],'',[0.5,3.5],[0.5,1.0,1.5,2.0,2.5,3.0,3.5],['','','','','','',''],
+                       r'$E-E_{\mathrm{FCI}}$ [$\mathrm{E_h}$]',[0,0.009],[0.000,0.003,0.006,0.009],
+                       ['0.000','0.003','0.006','0.009'],p=20.0,q=10.0)
+    fill_panel(ax[1,1],r'$R$ [\AA]',[0.5,3.5],[0.5,1.0,1.5,2.0,2.5,3.0,3.5],[0.5,1.0,1.5,2.0,2.5,3.0,3.5],
+                       r'$S^2$ [$\hbar$]',[0,2],[0.0,0.5,1.0,1.5,2.0],
+                       ['0.0','0.5','1.0','1.5','2.0'],p=20.0,q=10.0)
+    
+    # --------------------------------------------------
+    
+    h,l = ax[0,0].get_legend_handles_labels()
+    x0L,y0L,dxL,dyL = -0.02,1.05,2.20,0.20
+    ax[0,0].legend(h,l,fancybox=True,shadow=True,ncol=10,loc=3,
+                   bbox_to_anchor=(x0L,y0L,dxL,dyL),handlelength=2.5,handletextpad=1.0,columnspacing=2.0)
+    
+    fname = 'quccsd_reps_%d.eps' % depth
+    fig.savefig(fname,format='eps')
+    os.system('ps2epsi '+fname)
+    os.system('mv '+fname+'i '+fname)
+
---
+++ second_quantization/circuits/qUCCSD/uccsd_comparison.sh
@@ -0,0 +1,131 @@
+for flavor in 'restricted_closed_shell' 'unrestricted';
+do
+  for reverse in 'doubles_singles' 'singles_doubles';
+  do
+    for approximation in 'trotter' 'suzuki';
+    do
+      for R in 0.7 0.9 1.1 1.3 1.5 1.7 1.9 2.1 2.3 2.5 2.7 2.9 3.1 3.3;
+      do
+        for depth in 1;
+        do
+          cat > main.py <<***
+from qiskit                                        import *
+from qiskit.chemistry.drivers                      import UnitsType,HFMethodType
+from qiskit.chemistry.core                         import Hamiltonian,TransformationType,QubitMappingType
+from qiskit.chemistry.components.initial_states    import HartreeFock
+from qiskit.aqua.components.optimizers             import L_BFGS_B,COBYLA,CG
+from qiskit.aqua.algorithms                        import VQE
+from qiskit.aqua                                   import QuantumInstance,aqua_globals
+from qiskit.circuit.library                        import EfficientSU2
+
+import logging
+from   qiskit.chemistry import set_qiskit_chemistry_logging
+from   qiskit.aqua      import set_qiskit_aqua_logging
+set_qiskit_chemistry_logging(logging.INFO)
+set_qiskit_aqua_logging(logging.INFO)
+
+def print_circuit(fname,circuit):
+    outf = open(fname,'w')
+    for gate in circuit:
+        gate_name = gate[0].name
+        qubits = [q.index for q in gate[1]]
+        outf.write(str(gate_name)+' | '+' '.join([str(x) for x in qubits])+' | '+' '.join([str(x._symbol_expr) for x in gate[0].params])+'\n')
+    outf.close()
+
+def print_results(fname,res_vqe,res_ee,dN=0):
+    outf = open(fname,'w')
+    outf.write('%.12f %.12f \n' % (res_vqe[0],np.real(res_ee[0])))
+    outf.write('%.12f %.12f \n' % (res_vqe[1]+dN,np.real(res_ee[1]+dN)))
+    outf.write('%.12f %.12f \n' % (res_vqe[2],np.real(res_ee[2])))
+    outf.write('%.12f %.12f \n' % (res_vqe[3],np.real(res_ee[3])))
+    outf.close()
+
+import sys
+import numpy as np
+sys.path.append('/Users/mario/Documents/GitHub/QITE/qite_es/scf_calculations/subroutines')
+sys.path.append('/Users/mario/Documents/GitHub/QITE/qite_es/scf_calculations/qUCCSD')
+sys.path.append('/Users/mario/Documents/GitHub/QITE/qite_es/scf_calculations/subroutines/pyscfd')
+from pyscfdriver   import *
+if('${flavor}'=='restricted_closed_shell'):
+    from restricted_closed_shell_uccsd import UCCSD
+else:
+    from unrestricted_uccsd import UCCSD
+from utils         import *
+from CustomVarForm import *
+
+dist    = ${R}
+outfile = open('pes.txt','w')
+
+outfile.write("Bond distance: {}\n".format(dist))
+rho = np.load('/Users/mario/Documents/GitHub/QITE/qite_es/scf_calculations/SCF_FCI/h2o/res_fourth_pass.npy',allow_pickle=True).item()
+rho = rho[str(dist)]['rho_scf']
+z0 = 0.1177
+z1 = 0.47116
+y0 = 0.75545
+r0 = np.sqrt(y0**2 + (z0+z1)**2)
+ratio = dist/r0
+driver    = PySCFDriver(atom="O 0.0000 0.0000 {0}; H 0.0000 {1} -{2}; H 0.0000 -{1} -{2}".format(z0*ratio, y0*ratio, z1*ratio),
+                        unit=UnitsType.ANGSTROM,charge=0,spin=0,basis='sto-6g',hf_method=HFMethodType.RHF,symgroup='c2v',
+                        outfile=outfile,irrep_nelec={'A1':6,'A2':0,'B1':2,'B2':2},rho=rho)
+molecule  = driver.run()
+outfile.write("\nHartree-Fock energy %f \n" % (molecule.hf_energy))
+
+core      = Hamiltonian(transformation=TransformationType.FULL,qubit_mapping=QubitMappingType.PARITY,two_qubit_reduction=True,freeze_core=False,orbital_reduction=[0,1])
+H_op,A_op = core.run(molecule)
+
+z2syms,sqlist           = None,None
+H_op,A_op,z2syms,sqlist = taper(molecule,core,H_op,A_op,outfile)
+
+init_state = HartreeFock(num_orbitals=core._molecule_info['num_orbitals'],qubit_mapping=core._qubit_mapping,two_qubit_reduction=core._two_qubit_reduction,
+                         num_particles=core._molecule_info['num_particles'],sq_list=sqlist)
+
+outfile.write("\nHartree-Fock energy %f \n" % (molecule.hf_energy))
+outfile.write("\nHartree-Fock circuit\n")
+outfile.write(str(init_state.construct_circuit().draw())+"\n")
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+rev = ('${reverse}'=='doubles_singles')
+
+var_form  = UCCSD(num_orbitals=core._molecule_info['num_orbitals'],num_particles=core._molecule_info['num_particles'],
+                  active_occupied=None,active_unoccupied=None,initial_state=init_state,qubit_mapping=core._qubit_mapping,
+                  two_qubit_reduction=core._two_qubit_reduction,num_time_slices=1,z2_symmetries=z2syms,
+                  reverse_excitations=rev,expansion_mode='${approximation}',reps=${depth})
+optimizer = COBYLA(maxiter=0)
+depth = ${depth}
+if(depth==1): p0 = np.loadtxt('/Users/mario/Documents/GitHub/QITE/qite_es/scf_calculations/qUCCSD/quccsd_flavors_h2o/${flavor}/${approximation}/ds_%s/R_%s/output_parameters.txt' % (str(rev),str(dist)))
+else:         p0 = np.loadtxt('/Users/mario/Documents/GitHub/QITE/qite_es/scf_calculations/qUCCSD/quccsd_flavors_h2o_reps_2/${flavor}/${approximation}/ds_%s/R_%s/output_parameters.txt' % (str(rev),str(dist)))
+algo      = VQE(H_op,var_form,optimizer,aux_operators=A_op,include_custom=True,initial_point=p0)
+backend          = Aer.get_backend('statevector_simulator')
+quantum_instance = QuantumInstance(backend=backend)
+algo_result      = algo.run(quantum_instance)
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+p1 = algo._ret['opt_params']
+np.savetxt("output_parameters.txt",p1)
+
+res_vqe,res_ee = get_results(H_op,A_op,molecule,core,algo_result,outfile)
+np.save('results.npy',{'hf_results':molecule.hf_energy,'num_qubits':H_op.num_qubits,'num_parameters':var_form.num_parameters,'results_vqe':res_vqe,'results_eigensolver':res_ee},allow_pickle=True)
+
+outfile.write("\nCircuit\n")
+outfile.write(str(algo.get_optimal_circuit().draw())+"\n")
+
+Stot = A_op[1]
+for Ei,Oi in zip(var_form.excitations,var_form._hopping_ops):
+    comm = (Stot*Oi-Oi*Stot).chop(1e-10)
+    print("Excitation ",Ei," commutes with total spin ",comm.is_empty())
+
+p1 = algo._ret['opt_params']
+res_vqe,res_ee = get_results(H_op,A_op,molecule,core,algo_result,outfile)
+depth = ${depth}
+print_results('${flavor}/${reverse}/${approximation}/h2o_reps_%d_%s_results.txt' % (depth,str(dist)),res_vqe,res_ee,dN=4)
+circuit = algo.get_optimal_circuit()
+print_circuit('${flavor}/${reverse}/${approximation}/h2o_reps_%d_%s_circuit.txt' % (depth,str(dist)),circuit)
+***
+          python main.py
+        done
+      done
+    done
+  done
+done
