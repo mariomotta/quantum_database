0f5a2b3b4a519c7bab8ec4190cca2df494ad2c0e
Mario Motta <mario@marios-mbp.almaden.ibm.com>
2022-08-20 16:47:41 +0200

    cleanup

--- first_quantization/pad/variation_after_projection/operators_ry/run_calculations.sh
+++
@@ -1,7 +0,0 @@
-conda deactivate
-conda activate qiskit2021
-python gather_bh_operators.py > tmp
-python gather_hf_operators.py > tmp
-python gather_beh2_operators.py > tmp
-python gather_h2o_operators.py > tmp
-
